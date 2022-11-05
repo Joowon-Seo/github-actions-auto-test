a1590381eb08ef8a78fa87f31760f7466c164491
Juwon <tjwndnjs1998@gmail.com>
2022-11-05 19:11:17 +0900

    Initial commit

---
+++ src/main/java/com/example/githubactionsautotest/GithubActionsAutoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsautotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsAutoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsAutoTestApplication.class, args);
+	}
+
+}
